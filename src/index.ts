3ccfd97f334b1072596acfc9e539bdba4ea3653d
boykovm <vadim.boyko.01@outlook.com>
2021-07-06 18:37:01 +0300

    code was refactored

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,30 @@
 import {createServer, IncomingMessage, ServerResponse} from 'http'
 import * as fs from 'fs'
 import 'body-parser'
-const PATH = './file/superFile.txt'
 
-
-const hostname = '127.0.0.1'
-const port = 3000
+const PATH: string = './file/superFile.txt'
+const HOSTNAME: string = '127.0.0.1'
+const PORT: number = 3000
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    switch (req.url) {
-        case '/': {
-            if (req.method === 'GET'){
-                try {
-                    fs.readFile(PATH, "utf8", (e, data) => {
-                        if (e) {
-                            res.statusCode = 500
-                            res.end()
-                            throw e
-                        }
+    if (req.url === '/') {
+        switch (req.method) {
+            case 'GET': {
+                fs.readFile(PATH, "utf8", (e, data: string) => {
+                    if (e) {
+                        console.error(e)
+                        res.statusCode = 500
+                        res.end()
+                    } else {
                         res.statusCode = 200
                         res.setHeader('Content-Type', 'text/plain')
                         res.end(`${data}`)
-                    })
-                } catch (e) {
-                    console.error(e)
-                }
-
-            } else if (req.method === 'POST') {
-                let data = '';
+                    }
+                })
+                break
+            }
+            case 'POST': {
+                let data: string = '';
                 req.on('data', chunk => {
                     data += chunk;
                 })
@@ -37,65 +34,64 @@ const server = createServer((req: IncomingMessage, res: ServerResponse) => {
                         if (e) {
                             res.statusCode = 500
                             res.end();
-                            throw e
+                            console.error(e)
+                        } else {
+                            res.statusCode = 200
+                            res.end();
                         }
-                        res.statusCode = 200
-                        res.end();
                     })
                 })
-            } else if (req.method === 'PATCH') {
-                let data = ''
+                break
+            }
+            case 'PATCH': {
+                let data: string = ''
                 req.on('data', chunk => {
                     data += chunk
                 })
                 req.on('end', () => {
                     data = JSON.parse(data).data + '\n'
-                    try {
-                        if (fs.existsSync(PATH)) {
-                            fs.appendFile(PATH, data, (e) => {
-                                if (e) {
-                                    throw e
-                                }
+                    if (fs.existsSync(PATH)) {
+                        fs.appendFile(PATH, data, (e) => {
+                            if (e) {
+                                console.error(e)
+                                res.statusCode = 500
+                                res.end()
+                            } else {
                                 res.statusCode = 200
                                 res.end()
-                            })
-
-                            res.statusCode = 200
-                            res.end();
-                        } else {
-                            throw new Error('file not exists')
-                        }
-                    } catch (e) {
-                        console.error(e)
+                            }
+                        })
+                    } else {
                         res.statusCode = 500
-                        res.end();
-
+                        res.end()
                     }
                 })
-            } else if (req.method === 'DELETE'){
-                try {
-                    fs.unlink(PATH, (e) => {
-                        if (e) {
-                            throw e
-                        }
+                break
+            }
+            case 'DELETE': {
+                fs.unlink(PATH, (e) => {
+                    if (e) {
+                        res.statusCode = 500
+                        res.end()
+                        console.log(e)
+                    } else {
                         res.statusCode = 200
                         res.end()
-                    });
-                } catch (e) {
-                    console.error(e)
-                    res.statusCode = 500
-                    res.end()
-                }
-            } break
+                    }
+                });
+                break
+            }
+            default: {
+                res.statusCode = 404
+                res.end()
+            }
         }
-        default: {
-            res.statusCode = 404
-            res.end()
-        }
+    } else {
+        res.statusCode = 404
+        res.end()
     }
 })
 
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server was running at http://${HOSTNAME}:${PORT}/`)
 })

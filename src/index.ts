5744997b377c5a1015b71071c5c241c6f226fd2c
boykovm <vadim.boyko.01@outlook.com>
2021-07-07 19:36:12 +0300

    refactored with remarks

--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,99 @@
 import {createServer, IncomingMessage, ServerResponse} from 'http'
 import * as fs from 'fs'
 import 'body-parser'
+import {keys} from "./keys"
 
-const PATH: string = './file/superFile.txt'
-const HOSTNAME: string = '127.0.0.1'
-const PORT: number = 3000
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    if (req.url === '/') {
-        switch (req.method) {
-            case 'GET': {
-                fs.readFile(PATH, "utf8", (e, data: string) => {
-                    if (e) {
-                        console.error(e)
-                        res.statusCode = 500
-                        res.end()
-                    } else {
-                        res.statusCode = 200
-                        res.setHeader('Content-Type', 'text/plain')
-                        res.end(`${data}`)
-                    }
-                })
-                break
-            }
-            case 'POST': {
-                let data: string = '';
-                req.on('data', chunk => {
-                    data += chunk;
-                })
-                req.on('end', () => {
-                    data = JSON.parse(data).data + '\n'
-                    fs.writeFile(PATH, data, 'utf-8', (e) => {
-                        if (e) {
-                            res.statusCode = 500
-                            res.end();
-                            console.error(e)
-                        } else {
-                            res.statusCode = 200
-                            res.end();
-                        }
-                    })
-                })
-                break
-            }
-            case 'PATCH': {
-                let data: string = ''
-                req.on('data', chunk => {
-                    data += chunk
-                })
-                req.on('end', () => {
-                    data = JSON.parse(data).data + '\n'
-                    if (fs.existsSync(PATH)) {
-                        fs.appendFile(PATH, data, (e) => {
-                            if (e) {
-                                console.error(e)
-                                res.statusCode = 500
-                                res.end()
-                            } else {
-                                res.statusCode = 200
-                                res.end()
-                            }
-                        })
-                    } else {
-                        res.statusCode = 500
-                        res.end()
-                    }
-                })
-                break
-            }
-            case 'DELETE': {
-                fs.unlink(PATH, (e) => {
-                    if (e) {
-                        res.statusCode = 500
-                        res.end()
-                        console.log(e)
-                    } else {
-                        res.statusCode = 200
-                        res.end()
-                    }
-                });
-                break
-            }
-            default: {
-                res.statusCode = 404
-                res.end()
-            }
-        }
-    } else {
+    if (req.url !== '/') {
         res.statusCode = 404
         res.end()
+        return
     }
+    switch (req.method) {
+        case 'GET': {
+            fs.readFile(keys.PATH, keys.ENCODING, (e, data) => {
+                if (e) {
+                    console.error(e)
+                    res.statusCode = 500
+                    res.end()
+                    return
+                }
+                res
+                    .writeHead(200, {
+                        'Content-Type': 'text/plain'
+                    })
+                    .end(`${data}`)
+            })
+            break
+        }
+        case 'POST': {
+            let data: string = '';
+            req.on('data', chunk => {
+                data += chunk;
+            })
+            req.on('end', () => {
+                data = JSON.parse(data).data + '\n'
+                fs.writeFile(keys.PATH, data, keys.ENCODING, (e) => {
+                    if (e) {
+                        res.statusCode = 500
+                        res.end();
+                        console.error(e)
+                        return
+                    }
+                    res.statusCode = 200
+                    res.end();
+                })
+            })
+            break
+        }
+        case 'PATCH': {
+            let data: string = ''
+            req.on('data', chunk => {
+                data += chunk
+            })
+            req.on('end', () => {
+                data = JSON.parse(data).data + '\n'
+                if (fs.existsSync(keys.PATH)) {
+                    fs.appendFile(keys.PATH, data, (e) => {
+                        if (e) {
+                            console.error(e)
+                            res.statusCode = 500
+                            res.end()
+                            return
+                        }
+                        res.statusCode = 200
+                        res.end()
+                    })
+                    return
+                }
+                res.statusCode = 500
+                res.end()
+            })
+            break
+        }
+        case 'DELETE': {
+            fs.unlink(keys.PATH, (e) => {
+                if (e) {
+                    res.statusCode = 500
+                    res.end()
+                    console.log(e)
+                    return
+                }
+                res.statusCode = 200
+                res.end()
+            })
+            break
+        }
+        default: {
+            res.statusCode = 404
+            res.end()
+        }
+    }
+    return
 })
 
-server.listen(PORT, HOSTNAME, () => {
-    console.log(`Server was running at http://${HOSTNAME}:${PORT}/`)
+
+server.listen(keys.PORT, keys.HOSTNAME, () => {
+    console.log(`Server was running at http://${keys.HOSTNAME}:${keys.PORT}/`)
 })
